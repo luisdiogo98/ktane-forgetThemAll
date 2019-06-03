a01f19f72e35b4814858cb708926251a1cf1e446
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2019-06-03 20:59:04 +0000

    Initial build

---
+++ Assets/StageInfo.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using KModkit;
+
+public class StageInfo {
+
+	public int stageNo;
+	public int moduleId;
+
+	public bool[] LED = new bool[] {false, false, false, false, false, false, false, false, false, false, false, false, false};
+	public String moduleName;
+
+	public StageInfo(int stageNo, int moduleId, System.Random rnd)
+	{
+		this.stageNo = stageNo;
+		this.moduleId = moduleId;
+		
+		int LEDn = rnd.Next() % 3 + 1;
+
+		int[] colors = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+		colors = colors.OrderBy(x => rnd.Next()).ToArray();
+		for(int i = 0; i < LEDn; i++)
+		{
+			LED[colors[i]] = true;
+		}
+
+		Debug.LogFormat("[Forget Them All #{0}] Stage {1} LED: {2}", moduleId, stageNo, GetOnLED());
+	}
+
+	String GetOnLED()
+	{
+		String ret = "[";
+		for(int i = 0; i < LED.Count(); i++)
+		{
+			ret += LED[i] ? "1" : "0";
+			if(i != LED.Count() - 1)
+				ret += ", ";
+		}
+
+		return ret + "]";
+	}
+
+	public void SetModuleName(String moduleName)
+	{
+		this.moduleName = moduleName;
+
+		Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module is {2}.", moduleId, stageNo, moduleName);
+
+		if(moduleName.IndexOf("wire",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[0] = !LED[0];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module name contains \"wire\". Toggling Yellow LED.", moduleId, stageNo);
+		}
+
+		if(moduleName.IndexOf("button",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[1] = !LED[1];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module name contains \"button\". Toggling Grey LED.", moduleId, stageNo);
+		}
+		else if(moduleName.IndexOf("key",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[1] = !LED[1];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module name contains \"key\". Toggling Grey LED.", moduleId, stageNo);
+		}
+
+		if(moduleName.IndexOf("maze",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[2] = !LED[2];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"maze\". Toggling Blue LED.", moduleId, stageNo);
+		}
+
+		if(moduleName.IndexOf("simon",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[3] = !LED[3];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"simon\". Toggling Green LED.", moduleId, stageNo);
+		}
+
+		if(moduleName.IndexOf("morse",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[4] = !LED[4];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"morse\". Toggling Orange LED.", moduleId, stageNo);
+		}
+
+		if(moduleName.IndexOf("cruel",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[5] = !LED[5];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"cruel\". Toggling Red LED.", moduleId, stageNo);
+		}
+		else if(moduleName.IndexOf("complicated",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[5] = !LED[5];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"complicated\". Toggling Red LED.", moduleId, stageNo);
+		}
+		else if(moduleName.IndexOf("broken",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[5] = !LED[5];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"broken\". Toggling Red LED.", moduleId, stageNo);
+		}
+		else if(moduleName.IndexOf("cursed",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[5] = !LED[5];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"cursed\". Toggling Red LED.", moduleId, stageNo);
+		}
+		else if(moduleName.IndexOf("faulty",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[5] = !LED[5];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"faulty\". Toggling Red LED.", moduleId, stageNo);
+		}
+
+		if(moduleName.IndexOf("math",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[6] = !LED[6];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"math\". Toggling Lime LED.", moduleId, stageNo);
+		}
+		else if(moduleName.IndexOf("number",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[6] = !LED[6];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"number\". Toggling Lime LED.", moduleId, stageNo);
+		}
+		else if(moduleName.IndexOf("digit",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[6] = !LED[6];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"digit\". Toggling Lime LED.", moduleId, stageNo);
+		}
+		else if(moduleName.IndexOf("equation",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[6] = !LED[6];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"equation\". Toggling Lime LED.", moduleId, stageNo);
+		}
+		else if(moduleName.IndexOf("logic",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[6] = !LED[6];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"logic\". Toggling Lime LED.", moduleId, stageNo);
+		}
+
+		if(moduleName.IndexOf("word",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[7] = !LED[7];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"word\". Toggling Cyan LED.", moduleId, stageNo);
+		}
+		else if(moduleName.IndexOf("letter",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[7] = !LED[7];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"letter\". Toggling Cyan LED.", moduleId, stageNo);
+		}
+		else if(moduleName.IndexOf("phrase",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[7] = !LED[7];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"phrase\". Toggling Cyan LED.", moduleId, stageNo);
+		}
+		else if(moduleName.IndexOf("text",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[7] = !LED[7];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"text\". Toggling Cyan LED.", moduleId, stageNo);
+		}
+		else if(moduleName.IndexOf("talk",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[7] = !LED[7];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"talk\". Toggling Cyan LED.", moduleId, stageNo);
+		}
+		else if(moduleName.IndexOf("alphabet",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[7] = !LED[7];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"alphabet\". Toggling Cyan LED.", moduleId, stageNo);
+		}
+
+		if(moduleName.IndexOf("code",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[8] = !LED[8];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"code\". Toggling Brown LED.", moduleId, stageNo);
+		}
+		else if(moduleName.IndexOf("cypher",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[8] = !LED[8];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"cypher\". Toggling Brown LED.", moduleId, stageNo);
+		}
+
+		if(moduleName.IndexOf("light",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[9] = !LED[9];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"light\". Toggling White LED.", moduleId, stageNo);
+		}
+		else if(moduleName.IndexOf("LED",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[9] = !LED[9];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"LED\". Toggling White LED.", moduleId, stageNo);
+		}
+
+		if(moduleName.IndexOf("square",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[10] = !LED[10];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"square\". Toggling Purple LED.", moduleId, stageNo);
+		}
+		else if(moduleName.IndexOf("circle",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[10] = !LED[10];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"circle\". Toggling Purple LED.", moduleId, stageNo);
+		}
+		else if(moduleName.IndexOf("triangle",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[10] = !LED[10];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"triangle\". Toggling Purple LED.", moduleId, stageNo);
+		}
+		else if(moduleName.IndexOf("cube",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[10] = !LED[10];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"cube\". Toggling Purple LED.", moduleId, stageNo);
+		}
+		else if(moduleName.IndexOf("sphere",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[10] = !LED[10];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"sphere\". Toggling Purple LED.", moduleId, stageNo);
+		}
+
+		if(moduleName.IndexOf("color",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[11] = !LED[11];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"color\". Toggling Magenta LED.", moduleId, stageNo);
+		}
+		else if(moduleName.IndexOf("colour",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[11] = !LED[11];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"colour\". Toggling Magenta LED.", moduleId, stageNo);
+		}
+
+		if(moduleName.IndexOf("melody",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[12] = !LED[12];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"melody\". Toggling Pink LED.", moduleId, stageNo);
+		}
+		else if(moduleName.IndexOf("harmony",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[12] = !LED[12];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"harmony\". Toggling Pink LED.", moduleId, stageNo);
+		}
+		else if(moduleName.IndexOf("chord",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[12] = !LED[12];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"chord\". Toggling Pink LED.", moduleId, stageNo);
+		}
+		else if(moduleName.IndexOf("piano",  StringComparison.InvariantCultureIgnoreCase) != -1)
+		{
+			LED[12] = !LED[12];
+			Debug.LogFormat("[Forget Them All #{0}] Stage {1} corresponding module contains \"piano\". Toggling Pink LED.", moduleId, stageNo);
+		}
+
+		Debug.LogFormat("[Forget Them All #{0}] Stage {1} Actual LED (after evaluating broken): {2}", moduleId, stageNo, GetOnLED());
+
+	}
+}
---
+++ Assets/forgetThemAllScript.cs
@@ -0,0 +1,428 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using KModkit;
+
+public class forgetThemAllScript : MonoBehaviour 
+{
+	public KMBombInfo bomb;
+	public KMAudio Audio;
+
+	static System.Random rnd = new System.Random();
+	static String[] ignoredModules;
+
+	public TextMesh stageNo;
+
+	public GameObject[] wires;
+	public KMSelectable[] wireInt;
+	public Material[] wireColors;
+
+	public GameObject[] LEDs;
+	public Material[] lightColors;
+
+	//Logging
+	static int moduleIdCounter = 1;
+    int moduleId;
+    private bool moduleSolved;
+
+	int[] colors = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+
+	bool readyToSolve = false;
+
+	int stageCount;
+	int currentStage = 0;
+	List<int> wiresCut = new List<int>();
+	StageInfo[] stages;
+	int keyStage;
+	List<int> cutOrder = new List<int>();
+
+	int ticker = 0;
+	List<String> solvedModules = new List<String>();
+
+	int startTime;
+
+	void Awake()
+	{
+		moduleId = moduleIdCounter++;
+
+		if (ignoredModules == null)
+            ignoredModules = GetComponent<KMBossModule>().GetIgnoredModules("Forget Them All", new string[]{
+				"Forget Them All",
+				"Alchemy",
+				"Forget Everything",
+				"Forget Me Not",
+				"Forget This",
+				"Purgatory",
+				"Souvenir",
+				"Cookie Jars",
+				"Divided Squares",
+				"Hogwarts",
+				"The Swan",
+				"Turn the Keys",
+				"The Time Keeper",
+				"Timing is Everything",
+				"Turn the Key"
+            });
+
+		wireInt[0].OnInteract += delegate () { CutWire(0); return false; };
+		wireInt[1].OnInteract += delegate () { CutWire(1); return false; };
+		wireInt[2].OnInteract += delegate () { CutWire(2); return false; };
+		wireInt[3].OnInteract += delegate () { CutWire(3); return false; };
+		wireInt[4].OnInteract += delegate () { CutWire(4); return false; };
+		wireInt[5].OnInteract += delegate () { CutWire(5); return false; };
+		wireInt[6].OnInteract += delegate () { CutWire(6); return false; };
+		wireInt[7].OnInteract += delegate () { CutWire(7); return false; };
+		wireInt[8].OnInteract += delegate () { CutWire(8); return false; };
+		wireInt[9].OnInteract += delegate () { CutWire(9); return false; };
+		wireInt[10].OnInteract += delegate () { CutWire(10); return false; };
+		wireInt[11].OnInteract += delegate () { CutWire(11); return false; };
+		wireInt[12].OnInteract += delegate () { CutWire(12); return false; };
+	}
+
+	void Start () 
+	{
+		Debug.LogFormat("[Forget Them All #{0}] LED reference for logging purposes: [yellow, grey, blue, green, orange, red, lime, cyan, brown, white, purple, magenta, pink]", moduleId);
+
+		startTime = (int) (bomb.GetTime() / 60);
+
+		RandomizeColors();
+		if(CheckAutoSolve()) return;
+		CreateStages();
+		DisplayNextStage();
+	}
+	
+	void Update () 
+	{
+		ticker++;
+
+		if(ticker == 5)
+		{
+			ticker = 0;
+
+			List<String> newSolves = bomb.GetSolvedModuleNames().ToList();
+			foreach (String d in ignoredModules) { newSolves.Remove(d); }
+			foreach (String d in solvedModules) { newSolves.Remove(d); }
+
+			if(newSolves.Count() == 0)
+				return;
+			
+			stages[currentStage - 1].SetModuleName(newSolves[0]);
+			solvedModules.Add(newSolves[0]);
+			DisplayNextStage();
+		}
+	}
+
+	void CutWire(int wire)
+	{
+		GetComponent<KMAudio>().PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.WireSnip, transform);
+		wireInt[wire].enabled = false;
+		wires[wire].transform.Find("default").gameObject.SetActive(false);
+		wires[wire].transform.Find("hl").gameObject.SetActive(false);
+		wires[wire].transform.Find("wireCut").gameObject.SetActive(true);
+
+		foreach(Renderer r in wires[wire].GetComponentsInChildren<Renderer>())
+			r.material = wireColors[colors[wire]];
+
+		wiresCut.Add(wire);
+
+		if(moduleSolved)
+			return;
+
+		if(!readyToSolve)
+		{
+			GetComponent<KMBombModule>().HandleStrike();
+			Debug.LogFormat("[Forget Them All #{0}] Strike! {1} wire cut before module is ready to be solved.", moduleId, GetColorName(colors[wire]));
+		}
+
+		int index = cutOrder.FindIndex(x => x == colors[wire]);
+		cutOrder.RemoveAt(index);
+
+		LEDs[wire].transform.Find("light").GetComponentInChildren<Renderer>().material = lightColors[13];
+
+		if(index == 0)
+		{
+			if(cutOrder.Count() == 0)
+			{
+				moduleSolved = true;
+				stageNo.text = "";
+				Debug.LogFormat("[Forget Them All #{0}] Successfully cut {1} wire. Module solved.", moduleId, GetColorName(colors[wire]));
+				GetComponent<KMBombModule>().HandlePass();
+			}
+			else
+			{
+				Debug.LogFormat("[Forget Them All #{0}] Successfully cut {1} wire. Remaining wires order: {2}.", moduleId, GetColorName(colors[wire]), ListToString(cutOrder));
+			}
+		}
+		else
+		{
+			Debug.LogFormat("[Forget Them All #{0}] Strike! {1} wire cut. Expecting {2} wire. Remaining wires order: {3}.", moduleId, GetColorName(colors[wire]), GetColorName(colors[cutOrder[0]]), ListToString(cutOrder));
+			GetComponent<KMBombModule>().HandleStrike();
+		}
+	}
+
+	String GetColorName(int color)
+	{
+		switch(color)
+		{
+			case 0:
+				return "Yellow";
+			case 1:
+				return "Grey";
+			case 2:
+				return "Blue";
+			case 3:
+				return "Green";
+			case 4:
+				return "Orange";
+			case 5:
+				return "Red";
+			case 6:
+				return "Lime";
+			case 7:
+				return "Cyan";
+			case 8:
+				return "Brown";
+			case 9:
+				return "White";
+			case 10:
+				return "Purple";
+			case 11:
+				return "Magenta";
+			case 12:
+				return "Pink";
+		}
+
+		return "";
+	}
+
+	void RandomizeColors()
+	{
+		colors = colors.OrderBy(x => rnd.Next()).ToArray();
+
+		for(int i = 0; i < wires.Count(); i++)
+		{
+			foreach(Renderer r in wires[i].GetComponentsInChildren<Renderer>())
+				r.material = wireColors[colors[i]];
+		}
+	}
+
+	bool CheckAutoSolve()
+	{
+		stageCount = bomb.GetSolvableModuleNames().Where(x => !ignoredModules.Contains(x)).Count();
+		if(stageCount == 0)
+		{
+			moduleSolved = true;
+			GetComponent<KMBombModule>().HandlePass();
+			Debug.LogFormat("[Forget Them All #{0}] No not ignored modules on this bomb. Autosolving.", moduleId);
+			return true;
+		}
+
+		return false;
+	}
+
+	void CreateStages()
+	{
+		stages = new StageInfo[stageCount];
+		for(int i = 0; i < stages.Count(); i++)
+		{
+			stages[i] = new StageInfo(i+1, moduleId, rnd);
+		}
+	}
+
+	void DisplayNextStage()
+	{
+		currentStage++;
+		if(currentStage > stageCount)
+		{
+			readyToSolve = true;
+			
+			ShowFinalStage();
+
+			if(wiresCut.Count() == 13)
+			{
+				GetComponent<KMBombModule>().HandlePass();
+				Debug.LogFormat("[Forget Them All #{0}] All wires cut before module is ready to be solved. Autosolving. (Like, seriously?? 13 strikes and you're still alive? Jeez...)", moduleId);
+			}
+
+			CalcFinalSolution();
+			CalcWireOrder();
+			return;
+		}
+
+		foreach(GameObject LED in LEDs)
+		{
+			LED.transform.Find("light").GetComponentInChildren<Renderer>().material = lightColors[13];
+		}
+
+		StageInfo s = stages[currentStage - 1];
+
+		for(int i = 0; i < s.LED.Count(); i++)
+		{
+			if(s.LED[i])
+			{
+				LEDs[Array.IndexOf(colors, i)].GetComponentInChildren<Renderer>().material = lightColors[i];
+			}
+		}
+
+		String stageText = "";
+
+		if(currentStage < 100)
+			stageText += "0";
+		if(currentStage < 10)
+			stageText += "0";
+		stageText += currentStage + "";
+
+		stageNo.text = stageText;
+	}
+
+	void ShowFinalStage()
+	{
+		stageNo.text = "---";
+
+		for(int i = 0; i < 13; i++)
+		{
+			LEDs[Array.IndexOf(colors, i)].GetComponentInChildren<Renderer>().material = lightColors[i];
+		}
+	}
+
+	void CalcFinalSolution()
+	{
+		int[] totalLED = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+		foreach(StageInfo si in stages)
+		{
+			totalLED = totalLED.Select((x, index) => x + (si.LED[index] ? 1 : 0)).ToArray();
+		}
+
+		int aaBattery = bomb.GetBatteryCount(Battery.AA);
+		int portPlates = bomb.GetPortPlateCount();
+		int dupPorts = bomb.CountDuplicatePorts();
+		int moduleCount = bomb.GetModuleNames().Count();
+		int strikeCount = bomb.GetStrikes();
+		int snTotal = bomb.GetSerialNumberNumbers().Sum();
+		int snLetters = bomb.GetSerialNumberLetters().Count();
+		int portTypes = bomb.CountUniquePorts();
+		int onInd = bomb.GetOnIndicators().Count();
+		int offInd = bomb.GetOffIndicators().Count();
+		int dBattery = bomb.GetBatteryCount(Battery.D);
+
+		int value = totalLED[0] * aaBattery + 
+					totalLED[1] * portPlates + 
+					totalLED[2] * startTime + 
+					totalLED[3] * dupPorts + 
+					totalLED[4] * moduleCount + 
+					totalLED[5] * strikeCount + 
+					totalLED[6] * snTotal +
+					totalLED[7] * snLetters + 
+					totalLED[8] * portTypes + 
+					totalLED[9] * onInd + 
+					totalLED[10] * totalLED[10] +
+					totalLED[11] * offInd +
+					totalLED[12] * dBattery;
+
+		keyStage = value % stageCount;
+		if(keyStage == 0)
+			keyStage = stageCount;
+
+		Debug.LogFormat("[Forget Them All #{0}] Final value = {1}. Key stage is {2} - {3}.", moduleId, value, keyStage, stages[keyStage - 1].moduleName);
+	}
+
+	void CalcWireOrder()
+	{
+		List<int> wiresToCut = new List<int>();
+
+		for(int i = 0; i < 13; i++)
+		{
+			if(wiresCut.FindIndex(x => x == colors[i]) < 0)
+				wiresToCut.Add(colors[i]);
+		}
+
+		for(int i = 0; i < stages[keyStage - 1].moduleName.Length; i++)
+		{
+			int charColor = GetCharColor(stages[keyStage - 1].moduleName[i]);
+			int index = wiresToCut.FindIndex(x => x == charColor);
+			if(index >= 0)
+			{
+				cutOrder.Add(charColor);
+				wiresToCut.RemoveAt(index);
+			}
+		}
+
+		Debug.LogFormat("[Forget Them All #{0}] Wire cut order is {1}.", moduleId, ListToString(cutOrder));
+	}
+
+	int GetCharColor(char c)
+	{
+		char conv = Char.ToUpper(c);
+		switch(conv)
+		{
+			case 'A':
+			case 'N':
+			case '0':
+				return 0;
+			case 'B':
+			case 'O':
+			case '1':
+				return 1;
+			case 'C':
+			case 'P':
+			case '2':
+				return 2;
+			case 'D':
+			case 'Q':
+			case '3':
+				return 3;
+			case 'E':
+			case 'R':
+			case '4':
+				return 4;
+			case 'F':
+			case 'S':
+			case '5':
+				return 5;
+			case 'G':
+			case 'T':
+			case '6':
+				return 6;
+			case 'H':
+			case 'U':
+			case '7':
+				return 7;
+			case 'I':
+			case 'V':
+			case '8':
+				return 8;
+			case 'J':
+			case 'W':
+			case '9':
+				return 9;
+			case 'K':
+			case 'X':
+				return 10;
+			case 'L':
+			case 'Y':
+				return 11;
+			case 'M':
+			case 'Z':
+				return 12;
+		}
+
+		return -1;
+	}
+
+	String ListToString(List<int> l)
+	{
+		String res = "[";
+
+		for(int i = 0; i < l.Count(); i++)
+		{
+			res += l[i];
+			if(i != l.Count() - 1)
+				res += " ";
+		}
+
+		return res + "]";
+	}
+}
