359d8d6b149b028293ff0c61991bdd3facaadc05
Qkrisi <33597356+Qkrisi@users.noreply.github.com>
2019-07-06 19:51:43 +0200

    Updated help message

--- Assets/forgetThemAllScript.cs
+++ Assets/forgetThemAllScript.cs
@@ -457,7 +457,7 @@ public class forgetThemAllScript : MonoBehaviour
 
 		return res + "]";
 	}
-	public string TwitchHelpMessage = "Use '!{0} cycle' to cycle between homes. To submit a home use '!{0} <Home name>'. For ex. '!{0} Homestead'. Pay attention to the capitals in the name!";
+	public string TwitchHelpMessage = "Use '!{0} cut x y z' to cut wires! For ex. '!{0} cut 1 2 3 4 5 6' will cut wires 1, 2, 3, 4, 5 and 6 in that order.";
     IEnumerator ProcessTwitchCommand(string command)
 	{
 		string commfinal=command.Replace("cut ", "");
