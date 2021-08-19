e4346bedcbf097e2bcecb10469e9855d7d453061
Vincent Flyer (Vincent Chan) <35939457+VFlyer@users.noreply.github.com>
2021-08-18 19:37:31 -0700

    One More Fix?
    * 1 more hotfix, to account for when the module is ready to solve

--- Assets/Forget Them All/forgetThemAllScript.cs
+++ Assets/Forget Them All/forgetThemAllScript.cs
@@ -593,7 +593,7 @@ public class forgetThemAllScript : MonoBehaviour
 		if (keyStage == 0)
 			keyStage = stageCount;
 
-		Debug.LogFormat(!requireStageToSolve ? "[Forget Them All #{0}] Value up to this point: {1}" : "[Forget Them All #{0}] Final value: {1}. Key stage: {2} - {3}.", moduleId, value, keyStage, stages[keyStage - 1].moduleName);
+		Debug.LogFormat(requireStageToSolve || readyToSolve ? "[Forget Them All #{0}] Final value: {1}. Key stage: {2} - {3}." : "[Forget Them All #{0}] Value up to this point: {1}", moduleId, value, keyStage, stages[keyStage - 1].moduleName);
 	}
 
 	void CalcWireOrder()
